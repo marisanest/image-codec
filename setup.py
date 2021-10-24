ac1ede44daa1d44a9616ef14943127e57dcdf491
marisanest <marisanest@mailbox.org>
2021-10-24 12:04:41 +0200

    Update package setup.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import find_packages
-from setuptools import setup
+import pathlib
+
+from setuptools import find_packages, setup
 
 install_requires = [
     "click",
@@ -13,12 +14,20 @@ development_requires = ["black", "pytest"]
 
 setup(
     name="image-codec",
-    version="1.0",
-    description="Tool to encode and decode images.",
-    url="https://github.com/marisanest/image-codec",
+    version="1.0.0",
+    description="A simple CLI tool to encode and decode PGM images.",
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    keyword="image codec pgm",
     license="MIT",
+    url="https://github.com/marisanest/image-codec",
     packages=find_packages(include=["image_codec*"]),
+    python_requires=">=3.8",
     install_requires=install_requires,
     extras_require={"development": development_requires},
-    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "image-codec = image_codec.__main__:main",
+        ],
+    },
 )
